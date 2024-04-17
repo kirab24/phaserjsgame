5d355b59b1fb351b78f4ed11ff4113f4ae9e9050
Kira <breitenk@mail.gvsu.edu>
2024-04-17 15:40:47 -0400

    Fixed scene loop

--- WinScene.js
+++ WinScene.js
@@ -4,14 +4,17 @@ class WinScene extends Phaser.Scene {
     }
 
     create() {
+        // Create a semi-transparent overlay
         this.add.rectangle(500, 300, 1000, 600, 0x000000, 0.5);
 
+        // Display a "You Win!" message
         let winText = this.add.text(500, 200, 'You Win!', {
             fontFamily: '"Luckiest Guy"',
             fontSize: '64px',
             fill: '#fff'
         }).setOrigin(0.5);
 
+        // Button to go back to the main menu
         let mainMenuButton = this.add.text(500, 350, 'Main Menu', {
             fontFamily: '"Luckiest Guy"',
             fontSize: '32px',
